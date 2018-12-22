cac0fa6dc0265862fccdfdd53ed81bd2135eeb7a
yingshang26 <yingshang26@163.com>
2018-12-22 21:21:23 +0800

    添加IsInvalid属性

--- FirstWebBasedOnNetCore/FirstClsss.cs
+++ FirstWebBasedOnNetCore/FirstClsss.cs
@@ -7,5 +7,13 @@ namespace FirstWebBasedOnNetCore
 {
     public class FirstClsss
     {
+        /// <summary>
+        /// 是否无效
+        /// </summary>
+        public bool IsInvalid
+        {
+            get;
+            set;
+        }
     }
 }
